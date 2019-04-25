2fafd12eae6e661df0d14888e0e723fa09e95f80
itggot-markus-langnas <43438030+itggot-markus-langnas@users.noreply.github.com>
2019-04-25 13:42:32 +0200

    .
    .

---
+++ site/js/main.js
@@ -0,0 +1,7 @@
+function showMenu(event){
+    let menu = document.querySelector(".menu")
+    menu.classList.toggle("show")
+
+    let fadeLayer = document.querySelector(".fade-layer")
+    fadeLayer.classList.toggle("visible")
+}
